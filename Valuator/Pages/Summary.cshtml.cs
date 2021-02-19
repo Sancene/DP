db55fab2a8e90d9cf3eea2a0172f343b1b49865d
Sancene <minefattoproject@gmail.com>
2021-02-19 16:25:17 +0300

    lab 1 added

---
+++ Valuator/Pages/Summary.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Valuator.Pages
+{
+    public class SummaryModel : PageModel
+    {
+        private readonly ILogger<SummaryModel> _logger;
+        private readonly IStorage _storage;
+
+        public SummaryModel(ILogger<SummaryModel> logger, IStorage storage)
+        {
+            _logger = logger;
+            _storage = storage;
+        }
+
+        public double Rank { get; set; }
+        public double Similarity { get; set; }
+
+        public void OnGet(string id)
+        {
+            _logger.LogDebug(id);
+
+            Rank = Convert.ToDouble(_storage.Load("RANK-" + id.ToString()));
+            Similarity = Convert.ToDouble(_storage.Load("SIMILARITY-" + id.ToString()));
+        }
+    }
+}
